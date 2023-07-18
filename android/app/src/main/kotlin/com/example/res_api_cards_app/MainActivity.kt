a2b74d15d08c7eddf228075eb3ac70d67f6889ec
Mohd Kasim <kasimplecoder@email.com>
2023-07-18 21:19:35 +0530

    new changes done

---
+++ android/app/src/main/kotlin/com/example/res_api_cards_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.res_api_cards_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'features/posts/ui/posts_page.dart';
+
+
+
+
+void main() async{ 
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: PostsPage(),
+    );
+  }
+}
